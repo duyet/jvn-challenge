3312c2b09f0f17baf77bc75357d7a5cc127aaebd
duyetdev <lvduit08@gmail.com>
2017-04-24 23:24:50 +0700

    Update

--- competition/admin.py
+++ competition/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from models import Competition, CompetitionDataset
+from .models import Competition, CompetitionDataset
 
 admin.site.register(Competition)
 admin.site.register(CompetitionDataset)
